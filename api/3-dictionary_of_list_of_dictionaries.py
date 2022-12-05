05821dc3147cb40019e4644757ac20347f2397b3
Facub10 <facub1011@gmail.com>
2022-12-05 12:42:59 +0000

    task3 late

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""script that, using this REST API, for a given employee ID,"""
+"""returns information about his/her TODO list progress."""
+
+
+if __name__ == "__main__":
+    import json
+    from urllib import request
+
+    users_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                               "/users")
+    users = users_url.read().decode("utf-8")
+    users = json.loads(users)
+    f = open("todo_all_employees.json", "a")
+
+    user_json = {}
+    for user in users:
+        tasks_url = request.urlopen("https://jsonplaceholder.typicode.com"
+                                    "/todos?userId={}".format(user["id"]))
+        tasks = tasks_url.read().decode("utf-8")
+        tasks = json.loads(tasks)
+        user_json[user["id"]] = []
+        tasks_completed = []
+        for i in tasks:
+            if i["completed"]:
+                tasks_completed.append(i)
+        for task in tasks:
+            user_json[user["id"]].append({
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": user["username"]
+            })
+    f.write(json.dumps(user_json))
+    f.close()
