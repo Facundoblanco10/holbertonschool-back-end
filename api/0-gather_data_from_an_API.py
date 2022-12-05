1691e1ba418c51aec2329bed059c8d5015491ab9
Facub10 <facub1011@gmail.com>
2022-12-05 01:59:16 +0000

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python3
 """script that, using this REST API, for a given employee ID,"""
 """returns information about his/her TODO list progress."""
+import json
+from urllib import request
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    user_url = request.urlopen("https://jsonplaceholder.typicode.com"
+               "/users/{}".format(user_id))
+    tasks_url = request.urlopen("https://jsonplaceholder.typicode.com"
+               "/todos?userId={}".format(user_id))
+    user = user_url.read().decode("utf-8")
+    tasks = tasks_url.read().decode("utf-8")
+    user = json.loads(user)
+    tasks = json.loads(tasks)
+    tasks_completed = []
+    for i in tasks:
+        if i["completed"]:
+            tasks_completed.append(i)
+    print("Employee {} is done with tasks({}/{}):".format(user["name"],
+                                                          len(tasks_completed),
+                                                          len(tasks)))
+
+    for task in tasks_completed:
+        print("\t {}".format(task["title"]))
